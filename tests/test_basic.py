271b12cbc24028461f8a3a5ee4c198936e22ad92
alexa0o <alexa0o@yandex-team.ru>
2022-11-30 00:49:30 +0000

    test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -60,6 +60,15 @@ async def test_basic(service_client):
                 response = await service_client.get(
                         '/trykill?player_id={player_id}&x={x}&y={y}'.format(
                             player_id=turn, x=x, y=y))
+                if x == 0 and y == 0:
+                    assert response.text == "Miss"
+                elif x == 1 and y == 0:
+                    assert response.text == "Kill"
+                elif x == 0 and y == 8:
+                    assert response.text == "Damage"
+                elif x == 0 and y == 9:
+                    assert response.text == "Kill"
+
                 assert response.status == 200
 
     response = await service_client.get('/trykill?player_id=1&x=0&y=0')
